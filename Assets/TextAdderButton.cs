946a5fadd835ce2d2009d161d228e91c52a00c57
Gonzako <al341522@uji.es>
2020-04-11 22:50:15 +0200

    Worked a bit on keyboard-ception

---
+++ Assets/TextAdderButton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextAdderButton : MonoBehaviour
+{
+    private Button btn;
+
+    private void Start()
+    {
+        btn = GetComponent<Button>();
+        btn.onClick.AddListener(handleClick);
+    }
+
+    private void handleClick()
+    {
+
+    }
+}
---
+++ Assets/TextSetter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[ExecuteInEditMode]
+public class TextSetter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        char letter = transform.parent.name[transform.parent.name.Length-1];
+        GetComponent<TextMeshProUGUI>().text = letter.ToString();
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        char letter = transform.parent.name[transform.parent.name.Length - 1];
+        GetComponent<TextMeshProUGUI>().text = letter.ToString();
+    }
+}
